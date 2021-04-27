8d7806bef700bfc89f57d1c91df497e0be50e909
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-04-27 14:27:53 +0200

    all application Road added

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,11 @@ import express, { Request, Response } from "express";
 import * as core from "express-serve-static-core";
 import nunjucks from "nunjucks";
 import { GameModel } from "./models/game";
-import * as mongo from "mongodb";
 
 const clientWantsJson = (request: express.Request): boolean =>
   request.get("Accept") === "application/json";
 
-export function makeApp(db: mongo.Db): core.Express {
+export function makeApp(gameModel: GameModel): core.Express {
   const app = express();
 
   app.use("/assets", express.static("assets"));
@@ -28,48 +27,48 @@ export function makeApp(db: mongo.Db): core.Express {
   });
 
   app.get("/games", (request: Request, response: Response) => {
-    // gameModel.getAll().then((games) => {
-    //   if (clientWantsJson(request)) {
-    //     response.json(games)
-    //   } else {
-        response.render("games");
-    //   }
-    // });
+    gameModel.getAll().then((games) => {
+      if (clientWantsJson(request)) {
+        response.json(games)
+      } else {
+        response.render("games", { games });
+      }
+    });
   });
 
   app.get("/games/:game_slug", (request: Request, response: Response) => {
-    // gameModel.findBySlug(request.params.game_slug).then((game) => {
-    //   if (!game) {
-    //     response.status(404).end();
-    //   } else {
-    //     if (clientWantsJson(request)) {
-    //       response.json(game);
-    //     } else {
-          response.render("game_slug");
-    //     }
-    //   }
-    // });
+    gameModel.findBySlug(request.params.game_slug).then((game) => {
+      if (!game) {
+        response.status(404).end();
+      } else {
+        if (clientWantsJson(request)) {
+          response.json(game);
+        } else {
+          response.render("game_slug", { game });
+        }
+      }
+    });
   });
 
   app.get("/platforms", (request: Request, response: Response) => {
-    // gameModel.getPlatforms().then((platforms) => {
-    //   if (clientWantsJson(request)) {
-    //     response.json(platforms)
-    //   } else {
-        response.render("platforms");
-    //   }
-    // });
+    gameModel.getPlatforms().then((platforms) => {
+      if (clientWantsJson(request)) {
+        response.json(platforms)
+      } else {
+        response.render("platforms", { platforms });
+      }
+    });
   });
 
   app.get("/platforms/:platform_slug", (request: Request, response: Response) => {
-    // gameModel.findByPlatform(request.params.platform_slug)
-    //   .then((gamesForPlatform) => {
-    //     if (clientWantsJson(request)) {
-    //       response.json(gamesForPlatform)
-    //     } else {
-          response.render("platform_slug"z);
-    //     }
-    //   });
+    gameModel.findByPlatform(request.params.platform_slug)
+      .then((gamesForPlatform) => {
+        if (clientWantsJson(request)) {
+          response.json(gamesForPlatform)
+        } else {
+          response.render("platform_slug", { gamesForPlatform });
+        }
+      });
   });
 
   app.get("/login", (request, response) => {
