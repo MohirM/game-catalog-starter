8a40753fc2458edbd9d5d90a2ded7e308f0f99f7
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-04-27 18:36:45 +0200

    road home updated

--- src/server.ts
+++ src/server.ts
@@ -14,14 +14,17 @@ export function makeApp(db: Db): core.Express {
   app.set("view engine", "njk");
 
   app.get("/", (request: Request, response: Response) => {
-    response.render("index");
+    response.render("home");
   });
+  
   app.get("/home", (request: Request, response: Response) => {
     response.render("home");
   });
+
   app.get("/games", (request: Request, response: Response) => {
     response.render("games");
   });
+
   app.get("/games/:game_slug", (request: Request, response: Response) => {
     response.render("games_slug");
   });
@@ -29,21 +32,22 @@ export function makeApp(db: Db): core.Express {
   app.get("/platforms", (request: Request, response: Response) => {
     response.render("platforms");
   });
-  app.get(
-    "/platforms/:platform_slug",
-    (request: Request, response: Response) => {
+  app.get("/platforms/:platform_slug",(request: Request, response: Response) => {
       response.render("platform_slug");
-    }
-  );
+    });
+
   app.get("/login", (request: Request, response: Response) => {
     response.render("login");
   });
+
   app.get("/logout", (request: Request, response: Response) => {
     response.render("logout");
   });
+
   app.get("/payement", (request: Request, response: Response) => {
     response.render("payement");
   });
+
   app.get("/*", (request: Request, response: Response) => {
     response.render("not-found");
   });
