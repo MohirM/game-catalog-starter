3a0360d625e4622dc172eaef5e030e771ac7e3ff
ilezmankiev06 <ilezmankiev6@gmail.com>
2021-04-27 22:08:17 +0200

    updating all routes and initialization of client intance

--- src/Controlers/getControlers.ts
+++ src/Controlers/getControlers.ts
@@ -15,10 +15,22 @@ export function getGamesBySlug(request: Request, response: Response): void {
     response.render("games_slug")
 }
 
-// export function getHome(request: Request, response: Response): void {
-//     response.render("home")
-// }
+export function getPlatforms(request: Request, response: Response): void {
+    response.render("platforms")
+}
 
-// export function getHome(request: Request, response: Response): void {
-//     response.render("home")
-// }
+export function getPlatformsBySlug(request: Request, response: Response): void {
+    response.render("platform_slug")
+}
+
+export function getLogout(request: Request, response: Response): void {
+    response.render("logout")
+}
+
+export function getPayment(request: Request, response: Response): void {
+    response.render("payment")
+}
+
+export function getAllOthers(request: Request, response: Response): void {
+    response.render("not-found")
+}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express, { Request, Response } from "express";
 import * as core from "express-serve-static-core";
-import { Db } from "mongodb";
+import { Db, MongoClient } from "mongodb";
 import nunjucks from "nunjucks";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import mongoSession from "connect-mongo";
+import OAuth2Client, {
+  OAuth2ClientConstructor,
+} from "@fewlines/connect-client";
 import * as getControlers from "./Controlers/getControlers"
 
 export function makeApp(db: Db): core.Express {
+  //export function makeApp(client: MongoClient): core.Express {
   const app = express();
 
   nunjucks.configure("views", {
@@ -13,6 +20,19 @@ export function makeApp(db: Db): core.Express {
   });
 
   app.set("view engine", "njk");
+
+  //////////////////////////////////////////
+  // Initialization of the client instance//
+  //////////////////////////////////////////
+  const oauthClientConstructorProps: OAuth2ClientConstructor = {
+    openIDConfigurationURL: "https://fewlines.connect.prod.fewlines.tech/.well-known/openid-configuration",
+    clientID: `${process.env.CONNECT_CLIENT_ID}`,
+    clientSecret: `${process.env.CONNECT_CLIENT_SECRET}`,
+    redirectURI: "https://localhost:3000/oauth/callback",
+    audience: "wdb2g2",
+    scopes: ["openid", "email"],
+  };
+  const oauthClient = new OAuth2Client(oauthClientConstructorProps)
 
   app.get("/", (request: Request, response: Response) => {
     response.render("index");
@@ -23,42 +43,62 @@ export function makeApp(db: Db): core.Express {
   //   response.render("home");
   // });
 
-  app.get("/home", getControlers.getHome)
+  app.get("/home", getControlers.getHome);
 
   // app.get("/games", (request: Request, response: Response) => {
   //   response.render("games");
   // });
 
-  app.get("/games", getControlers.getGames)
+  app.get("/games", getControlers.getGames);
 
   // app.get("/games/:game_slug", (request: Request, response: Response) => {
   //   response.render("games_slug");
   // });
 
-  app.get("/games/:game_slug", getControlers.getGamesBySlug)
+  app.get("/games/:game_slug", getControlers.getGamesBySlug);
 
-  app.get("/platforms", (request: Request, response: Response) => {
-    response.render("platforms");
-  });
-  app.get("/platforms/:platform_slug",(request: Request, response: Response) => {
-      response.render("platform_slug");
-    });
 
-  app.get("/login", (request: Request, response: Response) => {
-    response.render("login");
-  });
+  // app.get("/platforms", (request: Request, response: Response) => {
+  //   response.render("platforms");
+  // });
 
-  app.get("/logout", (request: Request, response: Response) => {
-    response.render("logout");
-  });
+  app.get("/platforms", getControlers.getPlatforms);
 
-  app.get("/payement", (request: Request, response: Response) => {
-    response.render("payement");
-  });
 
-  app.get("/*", (request: Request, response: Response) => {
-    response.render("not-found");
-  });
+  // app.get("/platforms/:platform_slug",(request: Request, response: Response) => {
+  //     response.render("platform_slug");
+  //   });
+
+  app.get("/platforms/:platform_slug", getControlers.getPlatformsBySlug);
+
+
+  // app.get("/login", (request: Request, response: Response) => {
+  //   response.render("login");
+  // });
+  
+  app.get("/platforms/:platform_slug", getControlers.getPlatformsBySlug);
+
+
+  // app.get("/logout", (request: Request, response: Response) => {
+  //   response.render("logout");
+  // });
+
+  app.get("/logout", getControlers.getLogout);
+
+
+  // app.get("/payment", (request: Request, response: Response) => {
+  //   response.render("payment");
+  // });
+
+  app.get("/payment", getControlers.getPayment);
+
+
+  // app.get("/*", (request: Request, response: Response) => {
+  //   response.render("not-found");
+  // });
+
+  app.get("/*", getControlers.getAllOthers);
+  
 
   return app;
 }
